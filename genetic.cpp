4b5d18b7612a60a8e455a5fb6a80f24d24bfadf0
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2022-04-22 21:15:47 +0430

    minor bug fixed

--- genetic.cpp
+++ genetic.cpp
@@ -84,9 +84,9 @@ void print_pop(chrom pop[POPULATION])
 chrom convert_str_2_chrom(char *chrom_str)
 {                 // Accepts a string of 1s & 0s to the length of GENE_COUNT.
     chrom chromo; // Will return the corresponding chromosome.
-    for (int i = 0; chrom_str[i]; i++)
+    for (int i = 0; i < GENE_COUNT; i++)
     {
-        if (i >= GENE_COUNT || (chrom_str[i] != '0' && chrom_str[i] != '1'))
+        if (chrom_str[i] != '0' && chrom_str[i] != '1')
         {
             printf("Error!\nInvalid string as a chromosome.\n");
             exit(EXIT_FAILURE);
